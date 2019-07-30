060f7333743d2f464a8ebf33a9ac06469bae1270
lshea <lshea@csumb.edu>
2019-07-29 22:42:32 -0400

    This is one in many

--- tools.js
+++ tools.js
@@ -68,7 +68,7 @@ module.exports = {
 //        user: "root",
 //        password: "", 
 //        database: "img_gallery"
- 			 host: us-cdbr-iron-east-02.cleardb.net,
+ 			 host: "us-cdbr-iron-east-02.cleardb.net",
        user: bfd091ead737ce,
        password: 70657166, 
        database: heroku_9c1ef6a58cc3025
